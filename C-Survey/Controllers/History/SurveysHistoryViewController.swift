6fea9b057262ef2e46bd7a4c70c3e97f75afab06
Đặng Quang Tuấn <tuan.dang@asiantech.vn>
2019-11-22 08:11:03 +0700

    Update Source code.

---
+++ C-Survey/Controllers/History/SurveysHistoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SurveysHistoryViewController.swift
+//  C-Survey
+//
+//  Created by MBA0051 on 11/21/19.
+//  Copyright © 2019 Tuan.Dang. All rights reserved.
+//
+
+import UIKit
+
+class SurveysHistoryViewController: UIViewController {
+
+    @IBOutlet weak var historySurveysTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ C-Survey/Controllers/History/SurveysHistoryViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SurveysHistoryViewModel.swift
+//  C-Survey
+//
+//  Created by MBA0051 on 11/21/19.
+//  Copyright © 2019 Tuan.Dang. All rights reserved.
+//
+
+import Foundation
+
+class SurveysHistoryViewModel {
+    
+}
